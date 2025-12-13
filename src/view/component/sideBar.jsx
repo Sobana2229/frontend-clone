6c9f8004fa79946076bacd66ad9d0437b1e4ec19
Sobana2229 <sobanapriyal005@gmail.com>
2025-12-13 14:46:56 +0530

    sidebar update

--- src/view/component/sideBar.jsx
+++ src/view/component/sideBar.jsx
@@ -417,7 +417,7 @@ function SideBar({ isSidebarOpen, setIsSidebarOpen }) {
                 {/* Help */}
                 <button 
                   className="w-[10%] flex items-center px-3 py-2.5 rounded-lg transition-all duration-200 hover:bg-[rgba(28,28,28,0.03)] mt-2"
-                  style={{ height: '300px'}}
+                  style={{ height: '250px'}}
                 >
                   <div className="flex items-center gap-3">
                     <img src={HelpIcon} alt="Help" className="w-5 h-5" style={{ opacity: 0.6 }} />
