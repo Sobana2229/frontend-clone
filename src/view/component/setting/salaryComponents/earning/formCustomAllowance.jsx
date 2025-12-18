dfa924760116b084e6b97eef43b0274da526e582
Sobana2229 <sobanapriyal005@gmail.com>
2025-12-19 00:45:14 +0530

    earnigs

--- src/view/component/setting/salaryComponents/earning/formCustomAllowance.jsx
+++ src/view/component/setting/salaryComponents/earning/formCustomAllowance.jsx
@@ -25,9 +25,9 @@ function FormCustomAllowance({formData, setFormData, handleInputChange}) {
     };
     
     return (
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
+        <div className="flex gap-8">
             {/* Left Column - Main Fields */}
-            <div className="space-y-4">
+            <div className="space-y-4" style={{ width: '750px', flexShrink: 0 }}>
                 {/* Earning Name */}
                 <div>
                     <ReuseableInput
